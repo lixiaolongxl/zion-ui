c64951fa9d9fee5ad2696f27c12fa5381ed7e09d
Admin <lxldevtest@163.com>
2021-06-13 21:15:55 +0800

    gengs

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,13 +1,12 @@
-// import { shallowMount } from '@vue/test-utils'
-// import HelloWorld from '@/components/HelloWorld.vue'
-// "test:watch": "vue-cli-service test:unit --watch",
-//     "test": "vue-cli-service test:unit",
-// describe('HelloWorld.vue', () => {
-//   it('renders props.msg when passed', () => {
-//     const msg = 'new message'
-//     const wrapper = shallowMount(HelloWorld, {
-//       props: { msg }
-//     })
-//     expect(wrapper.text()).toMatch(msg)
-//   })
-// })
+import { shallowMount } from '@vue/test-utils'
+import HelloWorld from '../../examples/components/HelloWorld.vue'
+
+describe('HelloWorld.vue', () => {
+  it('renders props.msg when passed', () => {
+    const msg = 'new message'
+    const wrapper = shallowMount(HelloWorld, {
+      props: { msg }
+    })
+    expect(wrapper.text()).toMatch(msg)
+  })
+})
